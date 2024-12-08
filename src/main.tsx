05b47cb13fdfe2fec7b7e71d69cc7216edd3b42c
Germans Prudivus <germans.prudivus@gmail.com>
2024-12-08 19:33:14 +0100

    CSS

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './reset.css';
-import './index.css';
 
 import { store } from './state/store.ts';
 import { Provider } from 'react-redux';
