7b09d6a4b6241beaa4331ca9fac4c60c73c411b1
Germans Prudivus <germans.prudivus@gmail.com>
2024-12-08 19:34:34 +0100

    Updates

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './reset.css';
+import './index.css';
 
 import { store } from './state/store.ts';
 import { Provider } from 'react-redux';
