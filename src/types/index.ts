c8c607b41d985d54f42c11368af4f1a0886c5aac
Germans Prudivus <germans.prudivus@gmail.com>
2025-02-04 14:15:07 +0100

    Bug fix

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export type { User } from './User';
+export type { User } from './user/User';
 
 export type { BoostCard } from './game/BoostCard';
 export type { UpgradeCard } from './game/UpgradeCard';
