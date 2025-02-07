b76cc13807aa9450e833b879970407fa00ac2501
Germans Prudivus <germans.prudivus@gmail.com>
2025-02-07 11:59:49 +0100

    New compiler test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    basicSsl(),
+  ],
   base: '/skinbits-web/',
 });
