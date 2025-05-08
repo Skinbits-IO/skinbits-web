1364f58b222260184db295325333535c9ce3ded1
Germans Prudivus <germans.prudivus@gmail.com>
2025-05-08 14:19:51 +0200

    Auth bug fix

--- src/features/auth/index.tsx
+++ src/features/auth/index.tsx
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../../state/store';
 import { FC, PropsWithChildren, useEffect } from 'react';
-import { useCookies } from 'react-cookie';
 import { setIsLoading, setUser } from '../../state/userSlice';
 import { getUser, uploadGameSession } from '../../api';
 import { useLogin, useUser } from '../../hooks';
@@ -21,7 +20,6 @@ const toIsoUtcNoMs = (d: Date = new Date()) =>
 export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
   const dispatch = useDispatch<AppDispatch>();
   const { isLoading } = useUser();
-  const [cookies] = useCookies(['accessToken', 'refreshToken']);
 
   const queryClient = useQueryClient();
   const addNotification = useStatusNotification();
@@ -52,36 +50,31 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
   useLogin(error);
 
   useEffect(() => {
-    if (cookies.accessToken && cookies.refreshToken) {
-      if (!isPending) {
-        if (data) {
-          const { user, userGameInfo } = data;
-          dispatch(setUser(user));
-          dispatch(setUserGameInfo(userGameInfo));
-          if (localStorage.getItem('pendingGameSession')) {
-            const raw = localStorage.getItem('pendingGameSession');
-            if (raw) {
-              try {
-                const pending = JSON.parse(raw);
-                addGameSessionMutation.mutate({
-                  ...pending,
-                  endTime: toIsoUtcNoMs(),
-                });
-              } catch {
-                localStorage.removeItem('pendingGameSession');
-                dispatch(setIsLoading(false));
-              }
+    if (!isPending) {
+      if (data) {
+        const { user, userGameInfo } = data;
+        dispatch(setUser(user));
+        dispatch(setUserGameInfo(userGameInfo));
+        if (localStorage.getItem('pendingGameSession')) {
+          const raw = localStorage.getItem('pendingGameSession');
+          if (raw) {
+            try {
+              const pending = JSON.parse(raw);
+              addGameSessionMutation.mutate({
+                ...pending,
+                endTime: toIsoUtcNoMs(),
+              });
+            } catch {
+              localStorage.removeItem('pendingGameSession');
+              dispatch(setIsLoading(false));
             }
-          } else {
-            dispatch(setIsLoading(false));
           }
+        } else {
+          dispatch(setIsLoading(false));
         }
       }
-    } else {
-      dispatch(setUser(null));
-      dispatch(setIsLoading(false));
     }
-  }, [cookies, data, isPending]);
+  }, [data, isPending]);
 
   if (isLoading) {
     return <Loader />;
