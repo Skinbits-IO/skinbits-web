7c5e60c40458e15dc9f785d3f9d17c040731723b
Germans Prudivus <96908432+GermanPrudivus@users.noreply.github.com>
2025-05-18 22:25:45 +0200

    Delete src/components/Loader directory

--- src/components/Loader/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Loader.module.css';
-
-export const Loader = () => {
-  return (
-    <div className={styles.background}>
-      <p className={styles.loader}>Loading...</p>
-    </div>
-  );
-};
