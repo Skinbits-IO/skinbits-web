7c686ad168cb53200c08324786505373bf139eed
Germans Prudivus <germans.prudivus@gmail.com>
2025-08-23 11:30:32 +0300

    Fixed CI

--- src/features/ton-payment/api/invoiceService.ts
+++ src/features/ton-payment/api/invoiceService.ts
@@ -5,7 +5,8 @@ export async function confirmTonInvoice(id: string, amount: number) {
   try {
     const response = await api.post(`/invoice/confirm-ton`, {
       payment_id: id,
-      //currency: 'TON',
+      currency: 'TON',
+      amount: amount,
     });
     return response.data;
   } catch (error) {
