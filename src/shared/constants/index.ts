709cea3e3efb9c121a9c650370e7f07fe7cc4a6a
Germans Prudivus <germans.prudivus@gmail.com>
2025-09-21 13:47:41 +0200

    Added tg analytics

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,4 +1,10 @@
 export { RANKS } from './ranks';
-export { API_BASE, WEB_SOCKET_URL, YOUTUBE_URL } from './server';
+export {
+  API_BASE,
+  WEB_SOCKET_URL,
+  YOUTUBE_URL,
+  TG_ANALYTICS_APP_NAME,
+  TG_ANALYTICS_TOKEN,
+} from './server';
 export { DONATIONS_PRICE, PREMIUM_PLANS } from './donation';
 export { RARITIES_COLOR_MAP } from './marketplace';
--- src/shared/constants/server.ts
+++ src/shared/constants/server.ts
@@ -2,3 +2,7 @@ const SERVER_URL = 'https://skinbits-api-production.up.railway.app/api';
 export const API_BASE = import.meta.env.DEV ? '/api' : SERVER_URL;
 export const WEB_SOCKET_URL = 'wss://skinbits-api-production.up.railway.app';
 export const YOUTUBE_URL = 'https://youtu.be/quqnpYiC6JA?si=IzGLalkVIkOH3Lz0';
+
+export const TG_ANALYTICS_TOKEN =
+  'eyJhcHBfbmFtZSI6InNraW5iaXRzIiwiYXBwX3VybCI6Imh0dHBzOi8vdC5tZS9za2luYml0c19ib3QiLCJhcHBfZG9tYWluIjoiaHR0cHM6Ly9za2luYml0cy13ZWItZGV2LXByb2R1Y3Rpb24udXAucmFpbHdheS5hcHAvIn0=!MjDYV8+fK2dV/VN3hbDBTgFs+Eb4Ya8hPAK5QFWhdvc=';
+export const TG_ANALYTICS_APP_NAME = 'skinbits';
