f66a82ff798973df7886158cc7ef136dd0527cb3
Germans Prudivus <96908432+GermanPrudivus@users.noreply.github.com>
2025-09-23 11:38:39 +0200

    Update server.ts

--- src/shared/constants/server.ts
+++ src/shared/constants/server.ts
@@ -1,6 +1,6 @@
 const SERVER_URL = 'https://skinbits-api-production-test.up.railway.app/api';
 export const API_BASE = import.meta.env.DEV ? '/api' : SERVER_URL;
-export const WEB_SOCKET_URL = 'wss://skinbits-api-production.up.railway.app';
+export const WEB_SOCKET_URL = 'wss://skinbits-api-production-test.up.railway.app';
 export const YOUTUBE_URL = 'https://youtu.be/quqnpYiC6JA?si=IzGLalkVIkOH3Lz0';
 
 export const TG_ANALYTICS_TOKEN =
